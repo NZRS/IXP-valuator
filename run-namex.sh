5b5dcea520a1f9db17214fad13e2c468906389ec
Sebastian Castro <sebastian@nzrs.net.nz>
2016-10-23 19:39:31 +0200

    Fix the Namex dataset

--- run-namex.sh
+++ run-namex.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python2 ixp-customer.py --msm-file italy-results.json \
-    --cust-file customer-networks.json \
-    --save-file italy-aggregated.json
+python2 ixp-customer.py --msm-file namex-results.json \
+    --network-file customer-networks.json \
+    --detection customer \
+    --save-file namex-aggregated.json
